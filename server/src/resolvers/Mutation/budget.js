c7fb5e2366eeb9cf4a036ae6515dedab86ab6152
Michael Corey <fishincorey@gmail.com>
2018-07-26 15:39:14 -0600

    first commit

---
+++ server/src/resolvers/Mutation/budget.js
@@ -0,0 +1,53 @@
+const { getUserId } = require('../../utils')
+
+const budget = {
+  async setBudget(parent, { label, note }, ctx, info) {
+    const userId = getUserId(ctx)
+    return ctx.db.mutation.createBudget(
+      {
+        data: {
+          label,
+          note,
+          author: {
+            connect: { id: userId },
+          },
+        },
+      },
+      info
+    )
+  },
+
+  async reviseBudget(parent, { id }, ctx, info) {
+    const userId = getUserId(ctx)
+    const budgetExists = await ctx.db.exists.Budget({
+      id,
+      author: { id: userId },
+    })
+    if (!budgetExists) {
+      throw new Error(`Budget not found or you're not the author`)
+    }
+
+    return ctx.db.mutation.updateBudget(
+      {
+        where: { id },
+        data: { label: "somenewLabel" },
+      },
+      info,
+    )
+  },
+
+  async removeBudget(parent, { id }, ctx, info) {
+    const userId = getUserId(ctx)
+    const budgetExists = await ctx.db.exists.Budget({
+      id,
+      author: { id: userId },
+    })
+    if (!budgetExists) {
+      throw new Error(`Budget not found or you're not the author`)
+    }
+
+    return ctx.db.mutation.deleteBudget({ where: { id } })
+  },
+}
+
+module.exports = { budget }
